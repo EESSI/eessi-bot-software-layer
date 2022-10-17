fb5d6a10cff6ed30e577a8792399593718078eb7
Thomas Röblitz <thomas.roblitz@uib.no>
2022-10-18 00:41:16 +0200

    Added steps to determine built artefacts
    An artefact is an EESSI tarball. The code determines the full path
    to the last successfully build artefact for each target (architecture).

--- tasks/deploy.py
+++ tasks/deploy.py
@@ -1,5 +1,6 @@
-import os
 import glob
+import os
+import re
 
 from tools import config
 from pyghee.utils import log
@@ -24,6 +25,67 @@ def determine_pr_dirs(pr_number):
 
     return pr_directories
 
+def determine_slurm_out(pr_dir):
+    """
+    Determine path to slurm*out file for a job given a job directory.
+    """
+    slurm_out = os.path.join(pr_dir,'slurm-%s.out' % os.path.basename(pr_dir))
+    print("slurm out path = '%s'" % slurm_out)
+    return slurm_out
+
+
+def determine_eessi_tarballs(pr_dir):
+    """
+    Determine EESSI tarballs.
+    """
+    # determine all tarballs that are stored in the job directory
+    #   (only expecting 1)
+    tarball_pattern = 'eessi-*software-*.tar.gz'
+    glob_str = os.path.join(pr_dir,tarball_pattern)
+    eessi_tarballs = glob.glob(glob_str)
+    return eessi_tarballs
+
+
+def check_build_status(slurm_out, eessi_tarballs):
+    """
+    Check status of job in a given directory.
+    """
+    # analyse job result
+
+    # set some initial values
+    no_missing_modules = False
+    targz_created = False
+
+    # check slurm out for the below strings
+    #   ^No missing modules!$ --> software successfully installed
+    #   ^/eessi_bot_job/eessi-.*-software-.*.tar.gz created!$ -->
+    #     tarball successfully created
+    if os.path.exists(slurm_out):
+        re_missing_modules = re.compile('^No missing modules!$')
+        re_targz_created = re.compile('^/eessi_bot_job/eessi-.*-software-.*.tar.gz created!$')
+        outfile = open(slurm_out, "r")
+        for line in outfile:
+            if re_missing_modules.match(line):
+                # no missing modules
+                no_missing_modules = True
+            if re_targz_created.match(line):
+                # tarball created
+                targz_created = True
+
+    if no_missing_modules and targz_created and len(eessi_tarballs) == 1:
+        return True
+
+    return False
+
+
+def deploy_build(pd, build, ts):
+    """
+    Deploy build.
+    """
+    print("found build: %s/%s-%d.tar.gz" % (pd, build, ts))
+    # TODO run script eessi-upload-to-staging pd/build-ts.tar.gz
+
+
 def deploy_built_artefacts(pr, event_info):
     """
     Deploy built artefacts.
@@ -44,8 +106,42 @@ def deploy_built_artefacts(pr, event_info):
     pr_dirs = determine_pr_dirs(pr.number)
     print("pr_dirs = '%s'" % ','.join(pr_dirs))
 
+    # 2) for each build check the status of jobs (SUCCESS or FAILURE)
+    #    - scan slurm*out file for: No modules missing! & created messages
+    successes = []
+    for pr_dir in pr_dirs:
+        slurm_out = determine_slurm_out(pr_dir)
+        eessi_tarballs = determine_eessi_tarballs(pr_dir)
+        if check_build_status(slurm_out, eessi_tarballs):
+            print("SUCCESS: build in '%s' was successful" % pr_dir)
+            successes.append({ 'pr_dir': pr_dir, 'slurm_out': slurm_out, 'eessi_tarballs': eessi_tarballs })
+        else:
+            print("FAILURE: build in '%s' was NOT successful" % pr_dir)
 
-#    tarball_pattern = 
-#    for pr_dir in pr_dirs:
-#        eessi_tarballs = glob.glob(os.path.join(pr_dir,tarball_pattern))
+    # 3) for the successful ones, only use the last (use timestamp in
+    #    filename) for each software subdir
+    last_successful = {}
+    for sb in successes:
+        tbs = sb['eessi_tarballs']
+        print("num tarballs: %d" % len(tbs))
+        tb0 = tbs[0]
+        print("1st tarball: %s" % tb0)
+        tb0_base = os.path.basename(tb0)
+        print("tarball base: %s" % tb0_base)
+        build_pre = '-'.join(tb0_base.split('-')[:-1])
+        print("build_pre: %s" % build_pre)
+        timestamp = int(tb0_base.split('-')[-1][:-7])
+        print("timestamp: %d" % timestamp)
+        if build_pre in last_successful:
+            if last_successful[build_pre]['timestamp'] < timestamp:
+                last_successful[build_pre] = { 'pr_dir': sb['pr_dir'], 'timestamp': timestamp }
+        else:
+            last_successful[build_pre] = { 'pr_dir': sb['pr_dir'], 'timestamp': timestamp }
+
+    # 4) call function to deploy a single artefact per software subdir
+    for build in last_successful.keys():
+        pd = last_successful[build]['pr_dir']
+        ts = last_successful[build]['timestamp']
+        deploy_build(pd, build, ts)
+        print("found build: %s/%s-%d.tar.gz" % (pd, build, ts))
 
