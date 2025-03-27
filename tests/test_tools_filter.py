f40589d02e57c3a3b8e2809571f3640d484890ff
Thomas Roeblitz <thomas.roblitz@uib.no>
2025-03-27 23:46:51 +0100

    update tests for filters

--- tests/test_tools_filter.py
+++ tests/test_tools_filter.py
@@ -20,7 +20,7 @@ from tools.filter import (COMPONENT_TOO_SHORT,
                           COMPONENT_UNKNOWN,
                           EESSIBotActionFilter,
                           EESSIBotActionFilterError,
-                          FILTER_EMPTY_PATTERN,
+                          FILTER_EMPTY_VALUE,
                           FILTER_FORMAT_ERROR)
 
 
@@ -34,9 +34,9 @@ def test_empty_action_filter():
 def test_add_wellformed_filter_from_string():
     af = EESSIBotActionFilter("")
     component = 'acc'
-    pattern = 'nvidia/cc80'
-    af.add_filter_from_string(f"{component}:{pattern}")
-    expected = f"accelerator:{pattern}"
+    value = 'nvidia/cc80'
+    af.add_filter_from_string(f"{component}:{value}")
+    expected = f"accelerator:{value}"
     actual = af.to_string()
     assert expected == actual
 
@@ -52,29 +52,29 @@ def test_add_non_wellformed_filter_from_string():
     assert str(err1.value) == expected_msg1
 
     component2 = 'a'
-    pattern2 = 'zen4'
-    filter_string2 = f"{component2}:{pattern2}"
+    value2 = 'zen4'
+    filter_string2 = f"{component2}:{value2}"
     with pytest.raises(Exception) as err2:
         af.add_filter_from_string(filter_string2)
     assert err2.type == EESSIBotActionFilterError
-    expected_msg2 = COMPONENT_TOO_SHORT.format(component=component2, pattern=pattern2)
+    expected_msg2 = COMPONENT_TOO_SHORT.format(component=component2, value=value2)
     assert str(err2.value) == expected_msg2
 
     component3 = 'arc'
-    pattern3 = ''
-    filter_string3 = f"{component3}:{pattern3}"
+    value3 = ''
+    filter_string3 = f"{component3}:{value3}"
     with pytest.raises(Exception) as err3:
         af.add_filter_from_string(filter_string3)
     assert err3.type == EESSIBotActionFilterError
-    expected_msg3 = FILTER_EMPTY_PATTERN.format(filter_string=filter_string3)
+    expected_msg3 = FILTER_EMPTY_VALUE.format(filter_string=filter_string3)
     assert str(err3.value) == expected_msg3
 
 
 def test_add_single_action_filter():
     af = EESSIBotActionFilter("")
     component = 'arch'
-    pattern = '.*intel.*'
-    af.add_filter(component, pattern)
+    value = '.*intel.*'
+    af.add_filter(component, value)
     expected = "architecture:.*intel.*"
     actual = af.to_string()
     assert expected == actual
@@ -83,22 +83,22 @@ def test_add_single_action_filter():
 def test_add_non_supported_component():
     af = EESSIBotActionFilter("")
     component = 'machine'
-    pattern = '.*intel.*'
+    value = '.*intel.*'
     with pytest.raises(Exception) as err:
-        af.add_filter(component, pattern)
+        af.add_filter(component, value)
     assert err.type == EESSIBotActionFilterError
-    expected_msg = COMPONENT_UNKNOWN.format(component=component, pattern=pattern)
+    expected_msg = COMPONENT_UNKNOWN.format(component=component, value=value)
     assert str(err.value) == expected_msg
 
 
 def test_add_too_short_supported_component():
     af = EESSIBotActionFilter("")
     component = 'a'
-    pattern = '.*intel.*'
+    value = '.*intel.*'
     with pytest.raises(Exception) as err:
-        af.add_filter(component, pattern)
+        af.add_filter(component, value)
     assert err.type == EESSIBotActionFilterError
-    expected_msg = COMPONENT_TOO_SHORT.format(component=component, pattern=pattern)
+    expected_msg = COMPONENT_TOO_SHORT.format(component=component, value=value)
     assert str(err.value) == expected_msg
 
 
@@ -107,31 +107,31 @@ def test_add_too_short_supported_component():
 def complex_filter():
     af = EESSIBotActionFilter("")
     component1 = 'arch'
-    pattern1 = '.*intel.*'
-    af.add_filter(component1, pattern1)
+    value1 = '.*intel.*'
+    af.add_filter(component1, value1)
     component2 = 'repo'
-    pattern2 = 'nessi.no-2022.*'
-    af.add_filter(component2, pattern2)
+    value2 = 'nessi.no-2022.*'
+    af.add_filter(component2, value2)
     component3 = 'inst'
-    pattern3 = '[aA]'
-    af.add_filter(component3, pattern3)
+    value3 = '[aA]'
+    af.add_filter(component3, value3)
     yield af
 
 
 def test_remove_existing_filter(complex_filter):
     component1 = 'architecture'
-    pattern1 = '.*intel.*'
-    filter_string1 = f"{component1}:{pattern1}"
+    value1 = '.*intel.*'
+    filter_string1 = f"{component1}:{value1}"
     component2 = 'repository'
-    pattern2 = 'nessi.no-2022.*'
-    filter_string2 = f"{component2}:{pattern2}"
+    value2 = 'nessi.no-2022.*'
+    filter_string2 = f"{component2}:{value2}"
     component3 = 'instance'
-    pattern3 = '[aA]'
-    filter_string3 = f"{component3}:{pattern3}"
+    value3 = '[aA]'
+    filter_string3 = f"{component3}:{value3}"
 
     # remove last filter
     org_filter = copy.deepcopy(complex_filter)
-    org_filter.remove_filter(component3, pattern3)
+    org_filter.remove_filter(component3, value3)
     expected = filter_string1
     expected += f" {filter_string2}"
     actual = org_filter.to_string()
@@ -139,7 +139,7 @@ def test_remove_existing_filter(complex_filter):
 
     # remove second last filter
     org_filter = copy.deepcopy(complex_filter)
-    org_filter.remove_filter(component2, pattern2)
+    org_filter.remove_filter(component2, value2)
     expected = filter_string1
     expected += f" {filter_string3}"
     actual = org_filter.to_string()
@@ -147,7 +147,7 @@ def test_remove_existing_filter(complex_filter):
 
     # remove first filter
     org_filter = copy.deepcopy(complex_filter)
-    org_filter.remove_filter(component1, pattern1)
+    org_filter.remove_filter(component1, value1)
     expected = filter_string2
     expected += f" {filter_string3}"
     actual = org_filter.to_string()
@@ -156,11 +156,11 @@ def test_remove_existing_filter(complex_filter):
 
 def test_remove_non_existing_filter(complex_filter):
     component = 'accel'
-    pattern = 'amd/gfx90a'
+    value = 'amd/gfx90a'
 
     # remove non-existing filter
     org_filter = copy.deepcopy(complex_filter)
-    org_filter.remove_filter(component, pattern)
+    org_filter.remove_filter(component, value)
     org_filter_str = org_filter.to_string()
     complex_filter_str = complex_filter.to_string()
     assert org_filter_str == complex_filter_str
@@ -168,24 +168,24 @@ def test_remove_non_existing_filter(complex_filter):
 
 def test_remove_filter_errors(complex_filter):
     component1 = 'ac'
-    pattern1 = 'amd/gfx90a'
+    value1 = 'amd/gfx90a'
     component2 = 'operating_system'
-    pattern2 = 'linux'
+    value2 = 'linux'
 
     # remove filter using too short component name
     org_filter = copy.deepcopy(complex_filter)
     with pytest.raises(Exception) as err1:
-        org_filter.remove_filter(component1, pattern1)
+        org_filter.remove_filter(component1, value1)
     assert err1.type == EESSIBotActionFilterError
-    expected_msg1 = COMPONENT_TOO_SHORT.format(component=component1, pattern=pattern1)
+    expected_msg1 = COMPONENT_TOO_SHORT.format(component=component1, value=value1)
     assert str(err1.value) == expected_msg1
 
     # remove filter using unknown component name
     org_filter = copy.deepcopy(complex_filter)
     with pytest.raises(Exception) as err2:
-        org_filter.remove_filter(component2, pattern2)
+        org_filter.remove_filter(component2, value2)
     assert err2.type == EESSIBotActionFilterError
-    expected_msg2 = COMPONENT_UNKNOWN.format(component=component2, pattern=pattern2)
+    expected_msg2 = COMPONENT_UNKNOWN.format(component=component2, value=value2)
     assert str(err2.value) == expected_msg2
 
 
@@ -204,9 +204,9 @@ def test_check_matching_empty_filter():
 def test_check_matching_simple_filter():
     af = EESSIBotActionFilter("")
     component = 'arch'
-    pattern = '.*intel.*'
-    af.add_filter(component, pattern)
-    expected = f"architecture:{pattern}"
+    value = '.*intel.*'
+    af.add_filter(component, value)
+    expected = f"architecture:{value}"
     actual = af.to_string()
     assert expected == actual
 
@@ -256,8 +256,8 @@ def test_non_match_architecture_repository_context(complex_filter):
 def arch_filter_slash_syntax():
     af = EESSIBotActionFilter("")
     component = 'arch'
-    pattern = '.*/intel/.*'
-    af.add_filter(component, pattern)
+    value = '.*/intel/.*'
+    af.add_filter(component, value)
     yield af
 
 
@@ -277,8 +277,8 @@ def test_match_architecture_syntax_slash(arch_filter_slash_syntax):
 def arch_filter_dash_syntax():
     af = EESSIBotActionFilter("")
     component = 'arch'
-    pattern = '.*-intel-.*'
-    af.add_filter(component, pattern)
+    value = '.*-intel-.*'
+    af.add_filter(component, value)
     yield af
 
 
@@ -298,8 +298,8 @@ def test_match_architecture_syntax_dash(arch_filter_dash_syntax):
 def accel_filter_slash_syntax():
     af = EESSIBotActionFilter("")
     component = 'accel'
-    pattern = 'nvidia/.*'
-    af.add_filter(component, pattern)
+    value = 'nvidia/.*'
+    af.add_filter(component, value)
     yield af
 
 
@@ -319,8 +319,8 @@ def test_match_accelerator_syntax_slash(accel_filter_slash_syntax):
 def accel_filter_equal_syntax():
     af = EESSIBotActionFilter("")
     component = 'accel'
-    pattern = 'amd=gfx90a'
-    af.add_filter(component, pattern)
+    value = 'amd=gfx90a'
+    af.add_filter(component, value)
     yield af
 
 
