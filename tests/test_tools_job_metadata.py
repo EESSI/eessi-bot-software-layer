876b66230a8783f670a33cfffbc6db5ae8cfe457
Sondre B. Risanger <168830227+sondrebr@users.noreply.github.com>
2026-02-12 13:35:29 +0100

    Additional tests for tools/job_metadata.py

--- tests/test_tools_job_metadata.py
+++ tests/test_tools_job_metadata.py
@@ -9,9 +9,22 @@
 # license: GPLv2
 #
 
+# Standard library imports
 import os
 
-from tools.job_metadata import get_section_from_file, JOB_PR_SECTION
+# Local application imports (anything from EESSI/eessi-bot-software-layer)
+from tools.job_metadata import determine_job_id_from_job_directory, get_section_from_file, JOB_PR_SECTION
+
+
+def test_determine_job_id_from_job_directory(tmpdir):
+    logfile = os.path.join(tmpdir, "test_determine_job_id_from_job_directory.log")
+
+    job_dir = os.path.join(tmpdir, "5")
+    assert determine_job_id_from_job_directory(job_dir, logfile) == 5
+
+    # determine_job_id_from_job_directory() should return 0 for non-job dirs
+    not_job_dir = os.path.join(tmpdir, "not-a-job-dir")
+    assert determine_job_id_from_job_directory(not_job_dir, logfile) == 0
 
 
 def test_get_section_from_file(tmpdir):
@@ -20,14 +33,32 @@ def test_get_section_from_file(tmpdir):
     path = os.path.join(tmpdir, 'test.metadata')
     assert get_section_from_file(path, JOB_PR_SECTION, logfile) is None
 
+    # Reading an empty file should return an empty dictionary
+    with open(path, 'w') as fp:
+        pass
+    metadata_pr = get_section_from_file(path, JOB_PR_SECTION, logfile)
+    assert metadata_pr == {}
+
+    # Should return None if file exists but is invalid
+    with open(path, 'w') as fp:
+        fp.write("invalid format")
+    metadata_pr = get_section_from_file(path, JOB_PR_SECTION, logfile)
+    assert metadata_pr is None
+
+    # Write a valid metadata file
     with open(path, 'w') as fp:
         fp.write('''[PR]
         repo=test
-        pr_number=12345''')
+        pr_number=12345
+        pr_comment_id=23456
+        job_owner=user01''')
 
+    # Verify that the metadata file is read correctly
     metadata_pr = get_section_from_file(path, JOB_PR_SECTION, logfile)
     expected = {
         "repo": "test",
         "pr_number": "12345",
+        "pr_comment_id": "23456",
+        "job_owner": "user01",
     }
     assert metadata_pr == expected
