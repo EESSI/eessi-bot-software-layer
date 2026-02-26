c6523f5785b16d7a7a1e39f0ff70340deaff3bdf
Bob Dröge <b.e.droge@rug.nl>
2026-02-26 13:49:06 +0100

    split long line

--- tests/test_eessi_bot_job_manager.py
+++ tests/test_eessi_bot_job_manager.py
@@ -139,7 +139,12 @@ def test_determine_finished_jobs():
 
 def test_parse_scontrol_show_job_output():
     # Dummy output (shortened) from Slurm 25.11.3 for "scontrol show job <jobid>"
-    scontrol_output = '''JobId=123 JobName=bot_test_job UserId=eessibot(12345) MCS_label=N/A EligibleTime=Unknown AllocNode:Sid=my.node.name:123456 SubmitLine=/opt/slurm/25.11.3/bin/sbatch --hold --time=10-0:0:0 --nodes=1 --exclusive --cpus-per-task=1 --job-name=bot_test_job /home/eessibot/job.slurm WorkDir=/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv StdErr= StdIn=/dev/null StdOut=/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv/slurm-123.out TresPerTask=cpu=1'''
+    scontrol_output = 'JobId=123 JobName=bot_test_job UserId=eessibot(12345) MCS_label=N/A EligibleTime=Unknown' \
+                      ' AllocNode:Sid=my.node.name:123456 SubmitLine=/opt/slurm/25.11.3/bin/sbatch --hold' \
+                      ' --time=10-0:0:0 --nodes=1 --exclusive --cpus-per-task=1 --job-name=bot_test_job ' \
+                      '/home/eessibot/job.slurm WorkDir=/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/' \
+                      'generic/dev.eessi.io-riscv StdErr= StdIn=/dev/null StdOut=/jobs/2026.01/pr_123/' \
+                      'event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv/slurm-123.out TresPerTask=cpu=1'
     job_manager = EESSIBotSoftwareLayerJobManager()
     job_info = job_manager.parse_scontrol_show_job_output(scontrol_output)
     job_info_expected = {
