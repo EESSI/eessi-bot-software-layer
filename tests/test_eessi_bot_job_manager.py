a57b3c193a5d50be3712f3d4ed42a597d9c30553
Bob Dröge <b.e.droge@rug.nl>
2026-02-26 13:40:53 +0100

    add test for parse_scontrol_show_job_output

--- tests/test_eessi_bot_job_manager.py
+++ tests/test_eessi_bot_job_manager.py
@@ -135,3 +135,25 @@ def test_determine_finished_jobs():
     assert job_manager.determine_finished_jobs(known_jobs, current_jobs_all_jobs) == []
     assert job_manager.determine_finished_jobs(known_jobs, current_jobs_one_job) == ['1', '2']
     assert job_manager.determine_finished_jobs(known_jobs, {}) == ['0', '1', '2']
+
+
+def test_parse_scontrol_show_job_output():
+    # Dummy output (shortened) from Slurm 25.11.3 for "scontrol show job <jobid>"
+    scontrol_output = '''JobId=123 JobName=bot_test_job UserId=eessibot(12345) MCS_label=N/A EligibleTime=Unknown AllocNode:Sid=my.node.name:123456 SubmitLine=/opt/slurm/25.11.3/bin/sbatch --hold --time=10-0:0:0 --nodes=1 --exclusive --cpus-per-task=1 --job-name=bot_test_job /home/eessibot/job.slurm WorkDir=/home/eessibot/shared/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv-2025.06-001 StdErr= StdIn=/dev/null StdOut=/home/eessibot/shared/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv-2025.06-001/slurm-123.out TresPerTask=cpu=1'''
+    job_manager = EESSIBotSoftwareLayerJobManager()
+    job_info = job_manager.parse_scontrol_show_job_output(scontrol_output)
+    job_info_expected = {
+        'JobId': '123',
+        'JobName': 'bot_test_job',
+        'UserId': 'eessibot(12345)',
+        'MCS_label': 'N/A',
+        'EligibleTime': 'Unknown',
+        'AllocNode:Sid': 'my.node.name:123456',
+        'SubmitLine': '/opt/slurm/25.11.3/bin/sbatch --hold --time=10-0:0:0 --nodes=1 --exclusive --cpus-per-task=1 --job-name=bot_test_job /home/eessibot/job.slurm',
+        'WorkDir': '/home/eessibot/shared/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv-2025.06-001',
+        'StdErr': '',
+        'StdIn': '/dev/null',
+        'StdOut': '/home/eessibot/shared/jobs/2026.01/pr_123/event_123-456-789/run_000/riscv64/generic/dev.eessi.io-riscv-2025.06-001/slurm-123.out',
+        'TresPerTask': 'cpu=1',
+    }
+    assert job_info == job_info_expected
